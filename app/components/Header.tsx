da04043cb56d0e0be7835d4205dfb3f4b5145f41
equationalapplications <info@equationalapplications.com>
2023-01-09 23:04:49 -0500

    style: lint prettier

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,21 @@
-import React, { memo } from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { theme } from '../core/theme';
+import React, { memo } from "react"
+import { StyleSheet, Text } from "react-native"
+
+import { theme } from "../core/theme"
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
-const Header = ({ children }: Props) => (
-  <Text style={styles.header}>{children}</Text>
-);
+const Header = ({ children }: Props) => <Text style={styles.header}>{children}</Text>
 
 const styles = StyleSheet.create({
   header: {
     fontSize: 26,
     color: theme.colors.primary,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingVertical: 14,
   },
-});
+})
 
-export default memo(Header);
+export default memo(Header)
--- app/components/Paragraph.tsx
+++ app/components/Paragraph.tsx
@@ -1,23 +1,22 @@
-import React, { memo } from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { theme } from '../core/theme';
+import React, { memo } from "react"
+import { StyleSheet, Text } from "react-native"
+
+import { theme } from "../core/theme"
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
-const Paragraph = ({ children }: Props) => (
-  <Text style={styles.text}>{children}</Text>
-);
+const Paragraph = ({ children }: Props) => <Text style={styles.text}>{children}</Text>
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 16,
     lineHeight: 26,
     color: theme.colors.secondary,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 14,
   },
-});
+})
 
-export default memo(Paragraph);
+export default memo(Paragraph)
