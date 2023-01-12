50791cd72980990cdac1eeb7d9b8c5f9442105bf
equationalapplications <info@equationalapplications.com>
2023-01-12 09:05:17 -0500

    feat: install react-query firebase
    and wrap App in QueryClientProvider

--- App.js
+++ App.js
@@ -3,18 +3,23 @@ import { StatusBar } from "expo-status-bar"
 import { StyleSheet } from "react-native"
 import { Provider as PaperProvider } from "react-native-paper"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { theme } from "./app/core/theme"
 import RootNavigator from "./app/navigation/RootNavigator"
 
+const queryClient = new QueryClient()
+
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <SafeAreaProvider style={styles.container}>
-        <RootNavigator />
-        <StatusBar style="auto" />
-      </SafeAreaProvider>
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider theme={theme}>
+        <SafeAreaProvider style={styles.container}>
+          <RootNavigator />
+          <StatusBar style="auto" />
+        </SafeAreaProvider>
+      </PaperProvider>
+    </QueryClientProvider>
   )
 }
 
