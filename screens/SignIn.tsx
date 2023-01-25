d9d138458eca7ee0ae335d0b82161baf8f155604
equationalapplications <info@equationalapplications.com>
2023-01-25 11:44:05 -0500

    feat: add GoogleAuthProvider

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,32 +1,54 @@
-import { StyleSheet } from "react-native"
+import * as Google from "expo-auth-session/providers/google"
+import * as WebBrowser from "expo-web-browser"
+import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth"
+import { useEffect } from "react"
+import { StyleSheet, Button } from "react-native"
 
-import EditScreenInfo from "../components/EditScreenInfo"
 import { Text, View } from "../components/Themed"
 import { RootStackScreenProps } from "../navigation/types"
 
+WebBrowser.maybeCompleteAuthSession()
+
 export default function SignIn({ navigation }: RootStackScreenProps<"SignIn">) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Sign In</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-        </View>
-    )
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: "800200662040-m7pub0f15vdappk7qoqm1fcl4rsahvov.apps.googleusercontent.com",
+  })
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params
+      const auth = getAuth()
+      const credential = GoogleAuthProvider.credential(id_token)
+      signInWithCredential(auth, credential)
+    }
+  }, [response])
+
+  return (
+    <View style={styles.container}>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync()
+        }}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
 })
