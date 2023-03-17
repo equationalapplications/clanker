d83691a83f82b8cdeb73d7184eb2f4a12391c776
equationalapplications <info@equationalapplications.com>
2023-03-17 08:28:13 -0400

    refactor: useMemo and useCallback to reduce jank

--- app/screens/Chat.tsx
+++ app/screens/Chat.tsx
@@ -1,5 +1,5 @@
 import { httpsCallable } from "firebase/functions"
-import { useEffect, useCallback } from "react"
+import { useEffect, useMemo, useCallback } from "react"
 import { StyleSheet, View } from "react-native"
 import { GiftedChat, User, IMessage, Avatar, Bubble } from "react-native-gifted-chat"
 import { useTheme } from "react-native-paper"
@@ -15,15 +15,18 @@ const getReply: any = httpsCallable(functions, "getReply")
 
 export default function Chat({ navigation }: RootTabScreenProps<"Chat">) {
   const user = useUser()
-  const uid = user?.uid ?? ""
+  const uid = useMemo(() => user?.uid ?? "", [user])
   const messages = useMessages()
   const { colors, roundness } = useTheme()
 
-  const chatUser: User = {
-    _id: uid,
-    name: user?.displayName ?? "",
-    avatar: user?.photoURL ?? "",
-  }
+  const chatUser = useMemo<User>(
+    () => ({
+      _id: uid,
+      name: user?.displayName ?? "",
+      avatar: user?.photoURL ?? "",
+    }),
+    [uid, user],
+  )
 
   useEffect(() => {
     // Configure Purchases
@@ -34,21 +37,43 @@ export default function Chat({ navigation }: RootTabScreenProps<"Chat">) {
     //   observerMode: false,
     //   useAmazon: false,
     // })
-  }, [])
+  }, [uid])
 
-  const onSend = useCallback(async (messages: IMessage[]) => {
-    const { _id, createdAt, text, user } = messages[0]
-    const message = {
-      _id,
-      createdAt: Date.parse(createdAt),
-      text,
-      user,
-    }
-    updateMessages(message)
-    const { data } = await getReply({ message: text })
-    const reply = data.reply
-    console.log("reply", reply)
-  }, [])
+  const onSend = useCallback(
+    async (messages: IMessage[]) => {
+      const { _id, createdAt, text, user } = messages[0]
+      const message = {
+        _id,
+        createdAt: Date.parse(createdAt),
+        text,
+        user,
+      }
+      updateMessages(message)
+      const { data } = await getReply({ message: text })
+      const reply = data.reply
+      console.log("reply", reply)
+    },
+    [getReply],
+  )
+
+  const renderBubble = useCallback(
+    (props) => {
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            left: { backgroundColor: colors.secondary },
+            right: { backgroundColor: colors.primary },
+          }}
+          textStyle={{
+            left: { color: colors.onSecondary },
+            right: { color: colors.onPrimary },
+          }}
+        />
+      )
+    },
+    [colors],
+  )
 
   return (
     <View
@@ -82,21 +107,7 @@ export default function Chat({ navigation }: RootTabScreenProps<"Chat">) {
           left: { color: colors.onTertiary, fontSize: 12 },
           right: { color: colors.background, fontSize: 12 },
         }}
-        renderBubble={(props) => {
-          return (
-            <Bubble
-              {...props}
-              wrapperStyle={{
-                left: { backgroundColor: colors.secondary },
-                right: { backgroundColor: colors.primary },
-              }}
-              textStyle={{
-                left: { color: colors.onSecondary },
-                right: { color: colors.onPrimary },
-              }}
-            />
-          )
-        }}
+        renderBubble={renderBubble}
       />
     </View>
   )
