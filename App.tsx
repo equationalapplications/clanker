8ff557d678ee62c3c8a8556e8a2c0fefc4710d30
equationalapplications <info@equationalapplications.com>
2023-03-21 15:21:49 -0400

    refactor: wrap app in paper alerts provider
    and error boundary

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import "expo-dev-client"
 import { StatusBar } from "expo-status-bar"
 import { ReactNode } from "react"
 import { Provider as PaperProvider, DefaultTheme } from "react-native-paper"
-//import { AlertsProvider } from "react-native-paper-alerts"
+import { AlertsProvider } from "react-native-paper-alerts"
 import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
 
 import { PurchasesProvider } from "./app/contexts/PurchasesProvider"
 import useCachedResources from "./app/hooks/useCachedResources"
 import Navigation from "./app/navigation"
-//import { ErrorBoundary } from "./app/screens/ErrorScreen/ErrorBoundary"
+import { ErrorBoundary } from "./app/screens/ErrorScreen/ErrorBoundary"
 import { CustomDefaultTheme } from "./app/theme"
 
 type PaperProviderProps = {
@@ -30,12 +30,12 @@ export default function App() {
       <SafeAreaProvider initialMetrics={initialWindowMetrics}>
         <PaperProvider theme={CustomDefaultTheme} {...(null as any as PaperProviderProps)}>
           <PurchasesProvider>
-            {/*<AlertsProvider {...(null as any as AlertsProviderProps)}>*/}
-            {/*<ErrorBoundary catchErrors="always">*/}
-            <Navigation theme={CustomDefaultTheme} />
-            <StatusBar />
-            {/*</ErrorBoundary>*/}
-            {/*</AlertsProvider>*/}
+            <AlertsProvider {...(null as any as AlertsProviderProps)}>
+              <ErrorBoundary catchErrors="always">
+                <Navigation theme={CustomDefaultTheme} />
+                <StatusBar />
+              </ErrorBoundary>
+            </AlertsProvider>
           </PurchasesProvider>
         </PaperProvider>
       </SafeAreaProvider>
