3047c15dd58a7cb59d35500a837b393e913076d3
equationalapplications <info@equationalapplications.com>
2023-03-29 15:29:52 -0400

    fix: use Text from Paper for Styled Text

--- app/components/StyledText.tsx
+++ app/components/StyledText.tsx
@@ -1,4 +1,4 @@
-import { Text, TextProps } from "react-native"
+import { Text, TextProps } from "react-native-paper"
 
 export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: "space-mono" }]} />
