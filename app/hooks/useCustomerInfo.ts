78efec0e07930f6e2578ac146613537a29a99b89
equationalapplications <info@equationalapplications.com>
2023-03-29 21:49:16 -0400

    feat: use setIsPremium for entitlement from RevenueCat

--- app/hooks/useCustomerInfo.ts
+++ app/hooks/useCustomerInfo.ts
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react"
 import Purchases, { CustomerInfo } from "react-native-purchases"
 
-import { platform, purchasesRevenueCatStripeUrl } from "../config/constants"
+import {
+  platform,
+  purchasesRevenueCatStripeUrl,
+  revenueCatPurchasesEntitlementId,
+} from "../config/constants"
 import useUser from "../hooks/useUser"
+import setIsPremium from "../utilities/setIsPremium"
+import useUserPrivate from "./useUserPrivate"
 
 const useCustomerInfo = (): CustomerInfo | null => {
   const user = useUser()
+  const userPrivate = useUserPrivate()
+  const isPremium = userPrivate?.isPremium
 
   const [customerInfo, setCustomerInfo] = useState<CustomerInfo>(null)
 
@@ -14,7 +22,14 @@ const useCustomerInfo = (): CustomerInfo | null => {
       try {
         if (platform === "ios" || platform === "android") {
           const customerInfoData = await Purchases.getCustomerInfo()
+          const revenuecatIsPremium =
+            customerInfoData?.entitlements?.active?.hasOwnProperty(
+              revenueCatPurchasesEntitlementId,
+            ) ?? false
           setCustomerInfo(customerInfoData)
+          if (revenuecatIsPremium == !isPremium) {
+            setIsPremium()
+          }
         } else if (platform === "web") {
           const idToken = await user?.getIdToken()
           const response = await fetch(purchasesRevenueCatStripeUrl, {
@@ -26,8 +41,12 @@ const useCustomerInfo = (): CustomerInfo | null => {
           })
           const customerInfoData = await response.json()
           const activeSubscriptions = customerInfoData?.subscriber?.subscriptions
+          const revenuecatIsPremium = activeSubscriptions?.length > 0 ?? false
           console.log(activeSubscriptions)
           setCustomerInfo(activeSubscriptions)
+          if (revenuecatIsPremium == !isPremium) {
+            setIsPremium()
+          }
         }
       } catch (e) {
         console.log(e)
