8ecd6a0abd062951cb7caf714ebb7dd0a7b41f6a
equationalapplications <info@equationalapplications.com>
2023-03-31 14:56:07 -0400

    refactor: do not use this fuction

--- app/utilities/setIsPremium.ts
+++
@@ -1,9 +0,0 @@
-import { httpsCallable } from "firebase/functions"
-
-import { functions } from "../config/firebaseConfig"
-
-const setIsPremiumFn: any = httpsCallable(functions, "setIsPremium")
-
-export default async function setIsPremium() {
-  await setIsPremiumFn()
-}
