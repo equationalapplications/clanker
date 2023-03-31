df1f271419cb39439c6a5b2263b5e5684833d56b
equationalapplications <info@equationalapplications.com>
2023-03-31 14:59:30 -0400

    feat: create useIsPremium hook
    get data from RevenueCat

--- app/hooks/useIsPremium.ts
+++ app/hooks/useIsPremium.ts
@@ -5,48 +5,49 @@ import { platform, revenueCatPurchasesStripeApiKey, revenueCatBaseUrl } from "..
 import useUser from "./useUser"
 
 export const useIsPremium = (): boolean | null => {
-    const user = useUser()
-    const [isPremium, setIsPremium] = useState<boolean | null>(null)
+  const user = useUser()
+  const [isPremium, setIsPremium] = useState<boolean | null>(null)
 
-    useEffect(() => {
-        const fetchPurchasesOfferings = async () => {
-            try {
-                if (platform === "ios" || platform === "android") {
-                    const offerings = await Purchases.getOfferings()
-                    if (offerings.current !== null && offerings.current.availablePackages.length !== 0) {
-                        setIsPremium(true)
-                    } else {
-                        setIsPremium(false)
-                    }
-                } else if (platform === "web") {
-                    const response = await fetch(
-                        revenueCatBaseUrl + "/subscribers/" + user.uid + "/offerings",
-                        {
-                            method: "GET",
-                            headers: {
-                                "Content-Type": "application/json",
-                                Authorization: `Bearer ${revenueCatPurchasesStripeApiKey}`,
-                            },
-                        },
-                    )
-                    const purchasesOfferingsData = await response.json()
-                    if (
-                        purchasesOfferingsData?.current_offering_id !== null &&
-                        purchasesOfferingsData?.current?.availablePackages?.length !== 0
-                    ) {
-                        setIsPremium(true)
-                    } else {
-                        setIsPremium(false)
-                    }
-                }
-            } catch (e) {
-                console.log(e, e.message)
-            }
+  useEffect(() => {
+    const fetchPurchasesOfferings = async () => {
+      try {
+        if (platform === "ios" || platform === "android") {
+          const customerInfo = await Purchases.getCustomerInfo()
+          const entitlements = customerInfo?.entitlements
+          if (entitlements && Object.keys(entitlements.active).length > 0) {
+            setIsPremium(true)
+          } else {
+            setIsPremium(false)
+          }
+        } else if (platform === "web") {
+          const response = await fetch(
+            revenueCatBaseUrl + "/subscribers/" + user.uid + "/offerings",
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${revenueCatPurchasesStripeApiKey}`,
+              },
+            },
+          )
+          const purchasesOfferingsData = await response.json()
+          if (
+            purchasesOfferingsData?.current_offering_id !== null &&
+            purchasesOfferingsData?.current?.availablePackages?.length !== 0
+          ) {
+            setIsPremium(true)
+          } else {
+            setIsPremium(false)
+          }
         }
-        if (user) {
-            fetchPurchasesOfferings()
-        }
-    }, [user])
+      } catch (e) {
+        console.log(e, e.message)
+      }
+    }
+    if (user) {
+      fetchPurchasesOfferings()
+    }
+  }, [user])
 
-    return isPremium
+  return isPremium
 }
