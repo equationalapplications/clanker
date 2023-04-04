d530ccd0e0250e42a3d4d83a34ffb8e716f30eb3
equationalapplications <info@equationalapplications.com>
2023-04-04 15:16:56 -0400

    fix: wrap app in QueryClientProvider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import "expo-dev-client"
 import { StatusBar } from "expo-status-bar"
-//import { ReactNode } from "react"
 import ErrorBoundary from "react-native-error-boundary"
-//import { Provider as PaperProvider, DefaultTheme } from "react-native-paper"
-//import { AlertsProvider } from "react-native-paper-alerts"
 import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
+import { QueryClientProvider, QueryClient } from "react-query"
 
 import useCachedResources from "./app/hooks/useCachedResources"
-//import Navigation from "./app/navigation"
 import RootNavigator from "./app/navigation/RootNavigator"
-//import { CustomDefaultTheme } from "./app/theme"
 import ThemeProvider from "./app/providers/ThemeProvider"
 
-//type PaperProviderProps = {
-//  children: ReactNode
-//  theme?: typeof DefaultTheme
-//}
+const queryClient = new QueryClient()
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
@@ -32,13 +25,10 @@ export default function App() {
       <ErrorBoundary onError={onError}>
         <SafeAreaProvider initialMetrics={initialWindowMetrics}>
           <ThemeProvider>
-            {/*<PaperProvider theme={CustomDefaultTheme} {...(null as any as PaperProviderProps)}>*/}
-            {/*<AlertsProvider >*/}
-            {/*<Navigation theme={CustomDefaultTheme} />*/}
-            <RootNavigator />
-            <StatusBar />
-            {/*</AlertsProvider>*/}
-            {/*</PaperProvider>*/}
+            <QueryClientProvider client={queryClient}>
+              <RootNavigator />
+              <StatusBar />
+            </QueryClientProvider>
           </ThemeProvider>
         </SafeAreaProvider>
       </ErrorBoundary>
