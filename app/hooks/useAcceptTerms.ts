fd4d728167e696935246c879a17664ab138288ed
equationalapplications <info@equationalapplications.com>
2023-04-08 09:37:18 -0400

    fix: remove optimistic update

--- app/hooks/useAcceptTerms.ts
+++ app/hooks/useAcceptTerms.ts
@@ -11,14 +11,14 @@ export function useAcceptTerms() {
   const queryClient = useQueryClient()
 
   const acceptTermsMutation = useMutation(() => acceptTermsFunction(), {
-    onMutate: () => {
-      // Optimistically update the cache with the new hasAcceptedTermsDate field value
-      const newUserPrivate = { ...userPrivate, hasAcceptedTermsDate: new Date() }
-      queryClient.setQueryData("userPrivate", newUserPrivate)
-
-      // Return a rollback function to revert the optimistic update if the mutation fails
-      return { userPrivate }
-    },
+    // onMutate: () => {
+    //   // Optimistically update the cache with the new hasAcceptedTermsDate field value
+    //   const newUserPrivate = { ...userPrivate, hasAcceptedTermsDate: new Date() }
+    //   queryClient.setQueryData("userPrivate", newUserPrivate)
+    //
+    //   // Return a rollback function to revert the optimistic update if the mutation fails
+    //   return { userPrivate }
+    // },
   })
 
   return acceptTermsMutation
