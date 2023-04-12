17b4574515a8b6ee13a17740607e498452344379
equationalapplications <info@equationalapplications.com>
2023-04-12 13:09:36 -0400

    refactor: use getIsPremium to fetch

--- app/hooks/useIsPremium.ts
+++ app/hooks/useIsPremium.ts
@@ -1,63 +1,20 @@
-import { User } from "firebase/auth"
-import { useEffect } from "react"
-import Purchases, { CustomerInfo } from "react-native-purchases"
+import { useEffect, useState } from "react"
 import { useQuery } from "react-query"
 
 import useUser from "./useUser"
-import {
-  platform,
-  revenueCatPurchasesStripeApiKey,
-  revenueCatBaseUrl,
-  revenueCatSubscribers,
-} from "../config/constants"
-import { Subscriber } from "../config/purchasesConfig"
+import { getIsPremium } from "../utilities/getIsPremium"
 
-interface PurchasesOfferingsData {
-  subscriber: Subscriber
-}
-
-const fetchPremiumStatus = async (user: User): Promise<boolean | null> => {
-  if (!user) {
-    return null
-  }
-
-  if (platform === "ios" || platform === "android") {
-    const customerInfo: CustomerInfo = await Purchases.getCustomerInfo()
-    const entitlements = customerInfo?.entitlements
-
-    return entitlements && Object.keys(entitlements?.active).length > 0
-  } else if (platform === "web") {
-    const response = await fetch(revenueCatBaseUrl + revenueCatSubscribers + "/" + user.uid, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${revenueCatPurchasesStripeApiKey}`,
-      },
-    })
-    const purchasesOfferingsData: PurchasesOfferingsData = await response.json()
-    const subscriber = purchasesOfferingsData?.subscriber
-    const entitlements = subscriber?.entitlements ?? {}
-
-    return entitlements && Object.keys(entitlements).length > 0
-  }
-
-  return null
-}
-
-export const useIsPremium = (): boolean | null => {
+export const useIsPremium = (): boolean => {
   const user = useUser()
+  const [staleTime, setStaleTime] = useState<number>(60) // 1 minute
 
-  const { data: isPremium, refetch } = useQuery<boolean | null>(
-    "isPremium",
-    () => fetchPremiumStatus(user),
-    {
-      enabled: !!user,
-      refetchOnWindowFocus: false,
-      retry: 3,
-      staleTime: 1000 * 60 * 60 * 1, // 1 hour
-      useErrorBoundary: true,
-    },
-  )
+  const { data: isPremium, refetch } = useQuery<boolean>("isPremium", getIsPremium, {
+    enabled: !!user,
+    refetchOnWindowFocus: false,
+    retry: 3,
+    staleTime,
+    useErrorBoundary: true,
+  })
 
   useEffect(() => {
     if (user) {
@@ -65,5 +22,13 @@ export const useIsPremium = (): boolean | null => {
     }
   }, [user, refetch])
 
-  return isPremium
+  useEffect(() => {
+    if (isPremium === true) {
+      setStaleTime(1800) // 30 minutes
+    } else {
+      setStaleTime(60) // 1 minute
+    }
+  }, [isPremium])
+
+  return !!isPremium
 }
