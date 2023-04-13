1f2b67f649f9ba391f00fa9b58012df8a875b15d
equationalapplications <info@equationalapplications.com>
2023-04-12 20:28:35 -0400

    feat: add disabled prop

--- app/components/LoadingIndicator.tsx
+++ app/components/LoadingIndicator.tsx
@@ -1,6 +1,16 @@
 import React from "react"
 import { ActivityIndicator } from "react-native-paper"
 
-const LoadingIndicator = () => <ActivityIndicator size="large" />
+interface Props {
+  disabled?: boolean
+}
+
+const LoadingIndicator = ({ disabled }: Props) => {
+  if (disabled) {
+    return null
+  }
+
+  return <ActivityIndicator size="large" />
+}
 
 export default LoadingIndicator
