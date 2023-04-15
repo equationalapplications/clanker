ffe4875595f424ee12427aef4682306da15cf9f2
equationalapplications <info@equationalapplications.com>
2023-04-15 18:52:04 -0400

    refactor: rename Bottom Tab types

--- app/screens/Settings.tsx
+++ app/screens/Settings.tsx
@@ -4,9 +4,9 @@ import { StyleSheet, View } from "react-native"
 import Button from "../components/Button"
 import CombinedSubscriptionButton from "../components/CombinedSubscriptionButton"
 import LoadingIndicator from "../components/LoadingIndicator"
-import { RootTabScreenProps } from "../navigation/types"
+import { BottomTabScreenProps } from "../navigation/types"
 
-export default function Settings({ navigation }: RootTabScreenProps<"Settings">) {
+export default function Settings({ navigation }: BottomTabScreenProps<"Settings">) {
   const [isLoading, setIsLoading] = useState(false)
 
   const onChangeIsLoading = (isLoading: boolean) => {
