3b8cf7200c3c6cf1e5c7a872d1825925cf01a7a9
equationalapplications <info@equationalapplications.com>
2023-04-15 18:49:51 -0400

    refactor: show header on stacks
    not on BottomTabNavigator

--- app/navigation/SettingsStackNavigator.tsx
+++ app/navigation/SettingsStackNavigator.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import React from "react"
 
 import { SettingsStackParamList } from "./types"
+import { CreditCounterIcon } from "../components/CreditCounterIcon"
+import { TabBarIcon } from "../components/TabBarIcon"
 import Profile from "../screens/Profile"
 import Settings from "../screens/Settings"
 
@@ -13,12 +15,20 @@ export function SettingsStackNavigator() {
       <SettingsStack.Screen
         name="Settings"
         component={Settings}
-        options={{ headerShown: false, title: "Settings" }}
+        options={({ navigation }) => ({
+          title: "Settings",
+          tabBarIcon: ({ color }) => <TabBarIcon name="edit" color={color} />,
+          headerRight: () => <CreditCounterIcon navigation={navigation} />,
+        })}
       />
       <SettingsStack.Screen
         name="Profile"
         component={Profile}
-        options={{ headerShown: false, title: "Edit Character" }}
+        options={({ navigation }) => ({
+          title: "Profile",
+          tabBarIcon: ({ color }) => <TabBarIcon name="edit" color={color} />,
+          headerRight: () => <CreditCounterIcon navigation={navigation} />,
+        })}
       />
     </SettingsStack.Navigator>
   )
