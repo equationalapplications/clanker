84206631bd25a2f0be0e9f3e4d96536432738246
equationalapplications <info@equationalapplications.com>
2023-04-17 12:16:41 -0400

    style: remove unneeded import

--- app/screens/Privacy.tsx
+++ app/screens/Privacy.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, Text, View } from "react-native"
+import { StyleSheet, ScrollView, View } from "react-native"
 import { Paragraph } from "react-native-paper"
 
 export default function Privacy() {
