e825311398f6c02c7c0d80c776912cef892425fb
equationalapplications <info@equationalapplications.com>
2023-04-17 18:27:35 -0400

    feat: add navigation state persistance

--- app/providers/ThemeProviderNavigationContainer.tsx
+++ app/providers/ThemeProviderNavigationContainer.tsx
@@ -3,17 +3,20 @@ import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
 } from "@react-navigation/native"
-import React, { useEffect, useState } from "react"
-import { useColorScheme } from "react-native"
+import { useEffect, useState } from "react"
+import { useColorScheme, Linking } from "react-native"
 import {
   Provider as PaperProvider,
   adaptNavigationTheme,
   MD3DarkTheme,
   MD3LightTheme,
 } from "react-native-paper"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { colorsLight, colorsDark } from "../config/constants"
+import { colorsLight, colorsDark, platform } from "../config/constants"
 import LinkingConfiguration from "../navigation/LinkingConfiguration"
+
+const PERSISTENCE_KEY = 'NAVIGATION_STATE_V1'
 
 const { LightTheme, DarkTheme } = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
@@ -41,8 +44,9 @@ const CombinedDarkTheme = {
 }
 
 export const ThemeProviderNavigationContainer = ({ children }) => {
+  const [isReady, setIsReady] = useState(false)
+  const [initialState, setInitialState] = useState()
   const deviceTheme = useColorScheme()
-
   const [theme, setTheme] = useState(
     deviceTheme === "dark" ? CombinedDarkTheme : CombinedDefaultTheme,
   )
@@ -55,9 +59,44 @@ export const ThemeProviderNavigationContainer = ({ children }) => {
     }
   }, [deviceTheme])
 
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (platform !== 'web' && initialUrl == null) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString ? JSON.parse(savedStateString) : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady])
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer linking={LinkingConfiguration} theme={theme}>
+      <NavigationContainer
+        initialState={initialState}
+        onStateChange={(state) =>
+          AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+        }
+        linking={LinkingConfiguration}
+        theme={theme}
+      >
         {children}
       </NavigationContainer>
     </PaperProvider>
