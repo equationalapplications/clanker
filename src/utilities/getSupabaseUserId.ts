8934566ddfeaaf4597a5c04bdd7a60b560c8001f
equationalapplications <info@equationalapplications.com>
2026-04-09 21:20:18 -0400

    style(utilities): fix indentation in getSupabaseUserId

--- src/utilities/getSupabaseUserId.ts
+++ src/utilities/getSupabaseUserId.ts
@@ -1,7 +1,7 @@
 import { getSupabaseSession } from '~/utilities/getSupabaseSession'
 
 export async function getSupabaseUserId(): Promise<string | null> {
-    const session = await getSupabaseSession()
-    if (!session?.user) return null
-    return session.user.id
+  const session = await getSupabaseSession()
+  if (!session?.user) return null
+  return session.user.id
 }
