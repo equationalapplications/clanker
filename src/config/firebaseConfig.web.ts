9891fc2871547697af92dec851409e74c5a9d11b
equationalapplications <info@equationalapplications.com>
2026-05-19 10:49:29 -0400

    feat(config): add generateEmbeddingFn httpsCallable to firebase config

--- src/config/firebaseConfig.web.ts
+++ src/config/firebaseConfig.web.ts
@@ -132,6 +132,7 @@ const getUserCharactersFn = httpsCallable(functionsInstance, 'getUserCharacters'
 const getPublicCharacterFn = httpsCallable(functionsInstance, 'getPublicCharacter')
 const wikiLlmFn = httpsCallable(functionsInstance, 'wikiLlm')
 const wikiSyncFn = httpsCallable(functionsInstance, 'wikiSync')
+const generateEmbeddingFn = httpsCallable(functionsInstance, 'generateEmbedding')
 const documentExtractFn = httpsCallable(functionsInstance, 'documentExtract')
 
 export type FirebaseUser = User
@@ -165,5 +166,6 @@ export {
   getPublicCharacterFn,
   wikiLlmFn,
   wikiSyncFn,
+  generateEmbeddingFn,
   documentExtractFn,
 }
